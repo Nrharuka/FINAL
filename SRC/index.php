d730d9589961821ba478b03123eb80ff55e47a1d
Nrharuka <2201169@s.asojuku.ac.jp>
2024-01-11 12:46:03 +0900

    1/11  メニュー画面作成中

--- SRC/index.php
+++ SRC/index.php
@@ -4,10 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/index.css">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Corben:700"> -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cherry+Swash:700">
     <title>おすすめカフェ管理システム</title>
 </head>
 <body>
     <h1>Recommended Cafe</h1>
     
+    <div class="wrapper">
+    <ul class="tab">
+    <li><a href="list.php">一覧</a></li>
+    <li><a href="registration.php">登録</a></li>
+    <li><a href="update.php">更新</a></li>
+    <li><a href="delete.php">削除</a></li>
+    </ul>
+    </div>
+    
+    <!-- <div class="search">
+            <form action="searchlist.php" method="post">
+                <input class="keyword" type="text" name="keyword" placeholder="  🔍       キーワード検索">
+                <input class="search2" type="submit" name="search" value="検索"><br><br>
+            </form>
+    </div> -->
+
 </body>
 </html>
